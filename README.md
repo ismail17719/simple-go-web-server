14993abdd98df42162dbb55fe80d0c8383a7d50a
= <=>
2022-11-20 11:14:45 +0500

    Simple web server completed

--- README.md
+++ README.md
@@ -2,5 +2,6 @@ This is a project that demonstrates how to create a simple Go web server. There 
 
 These are the routes:
 - / This route demonstrates how to return a static html file to a user request. In our case its index.html
-- /hello This route demonstrates how to call a function in response to a user request
-- /form This route shows basic form handling
+- /greet This route demonstrates how to call a function in response to a user request
+- /form.html to return form.html file in the static files directory. This file has basic login form and "admin" is the default username and password
+- /form This route shows basic form handling by first calling a function and then return an html file that contains the form.
--- main.go
+++ main.go
@@ -6,7 +6,52 @@ import (
 	"net/http"
 )
 
+func greetHandler(w http.ResponseWriter, r *http.Request){
+	if r.URL.Path != "/greet" {
+		http.Error(w, "404 - NOT FOUND", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method not supported", http.StatusBadRequest)
+		return
+	}
+	fmt.Fprint(w, "Ahoy there!!!")
+}
+
+func formHandler(w http.ResponseWriter, r *http.Request){
+	if r.URL.Path != "/form" {
+		http.Error(w, "404 - NOT FOUND", http.StatusNotFound)
+		return
+	}
+	if r.Method != "POST" {
+		http.Error(w, "Method not supported", http.StatusBadRequest)
+		return
+	}
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprint(w, "Form parsing error: %v", err)
+		return
+	}
+	fmt.Fprint(w, "POST request reach to server\n")
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+	if username =="admin" && password == "admin" {
+		fmt.Fprint(w, "You have successfully logged in")
+		return
+	} else {
+		fmt.Fprint(w, "Username or password is wrong")
+		return
+	}
+}
+
 func main()  {
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/greet", greetHandler)
 	
-	
+	//Start go server
+	fmt.Printf("Starting Go server at port 8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
